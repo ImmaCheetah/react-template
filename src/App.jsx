b029dbedb1a01c298e6a7d6f014e70c0ef538150
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-07-30 10:24:32 -0400

    add prop types and testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState, useEffect } from 'react'
-
+import PropTypes from 'prop-types';
 import './App.css'
 
-function App() {
+const RenderName = (props) => {
+  return <div>{props.name}</div>;
+};
 
-  
+RenderName.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
   return (
     <>
-      <p>hello</p>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
     </>
-  )
-}
+  );
+};
 
 export default App
